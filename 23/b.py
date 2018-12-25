43ccbbe156d545f2c302ec73f492acb7da274cee
Erik Odenman <eodenman@invidi.se>
2018-12-25 09:56:14 +0100

    Small bug fix

--- 23/b.py
+++ 23/b.py
@@ -8,7 +8,7 @@ q = PriorityQueue()
 for x,y,z,r in bots:
   d = abs(x) + abs(y) + abs(z)
   q.put((max(0, d - r),1))
-  q.put((d + r,-1))
+  q.put((d + r + 1,-1))
 count = 0
 maxCount = 0
 result = 0
